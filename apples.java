ca8600eaff99a6efad5530895a42d3da53056d4b
HypeCyborg <40946672+HypeCyborg@users.noreply.github.com>
2019-12-22 20:17:47 +0000

    Add files via upload

---
+++ apples.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class apples{
+public static void main(String [] args) {
+Scanner Harry = new Scanner (System.in);
+double fnum;
+double snum;
+double answer;
+System.out.println("Enter first num:  ");
+fnum = Harry.nextDouble();
+
+
+
+
+
+}
+
+
+}
