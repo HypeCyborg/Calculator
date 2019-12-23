33c1fe32393aaf9765a0d72c3970c47d37508cdc
HypeCyborg <40946672+HypeCyborg@users.noreply.github.com>
2019-12-23 17:16:08 +0000

    Update apples.java

--- apples.java
+++ apples.java
@@ -1,19 +1,34 @@
 import java.util.Scanner;
 
-class apples{
-public static void main(String [] args) {
-Scanner Harry = new Scanner (System.in);
-double fnum;
-double snum;
-double answer;
-System.out.println("Enter first num:  ");
-fnum = Harry.nextDouble();
+class Maths{
+public static void main(String[] args) {
+    Scanner Maths = new Scanner (System.in);
+    double fnum;
+    double snum;
+    double answer;
+    System.out.println("Enter first num:  ");
+    fnum = Maths.nextDouble();
+    System.out.println("Enter second num:  ");
+    snum = Maths.nextDouble();
+    answer = fnum + snum;
+    System.out.println(answer);
+
+
+
+
+    
+
+
+
 
 
 
 
 
 }
-
-
+    
 }
+
+
+
+
