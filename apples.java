e6df986a084a3a9ccd14c0ec0886450b888d1c10
HypeCyborg <40946672+HypeCyborg@users.noreply.github.com>
2020-06-28 20:25:49 +0100

    Add files via upload

--- apples.java
+++ apples.java
@@ -1,85 +1,53 @@
 import java.util.Scanner;
 
 class apples{
-public static void main(String[] args) {
-    Scanner  apples=new Scanner(System.in);
-    int fnum;
-    char sign;
-    int snum;
-    int answer;
-    
-     
-  
-System.out.println("Enter first num:  ");
-  fnum=apples.nextInt();
+    public static void main(String[] args) {
+        Scanner apples = new Scanner(System.in);
+        double fnum;
+        char sign;
+        double snum;
+        double answer;
 
-   
+        while(true) {  // Use a while loop instead of for loop
+            System.out.println("Enter first num:  ");
+            fnum=apples.nextDouble();
+          // putting an if statement here, "if it has either $£, then follow this route" kinda thing
 
+            System.out.println("Enter second num:  ");
+            snum=apples.nextDouble();
 
-    System.out.println("Enter second num:  ");
-    snum=apples.nextInt();
-   
+            System.out.print("Enter the mathematical operator to be used: ");
+            sign = apples.next().charAt(0);
 
-      
-    for ( ; ; ) {
-    System.out.print("Enter the mathematical operator to be used: ");
-    sign=apples.next().charAt(0);
+            switch(sign) {
+                  case '*':
+                    answer = fnum * snum;
+                    System.out.println(fnum + " " + sign + " " + snum +  "= " + " " + answer);
+                    break;
+                  case '/':
+                    answer = fnum / snum;
+                    System.out.println(fnum + " " + sign + " " + snum + " = " + " " + answer);
+                    break;
+                    case '-':
+                    answer = fnum - snum;
+                    System.out.println(fnum + " " + sign + " " + snum + " = " + " " + answer);
+                    case '+':
+                    answer = fnum + snum;
+                    System.out.println(fnum + " " + sign + " " + snum + " = " + " " + answer);
+                  default:
+                    System.out.println("Your Input is not correct,please check it for any error(s).");
+          }
+          
 
-    if (sign == '+') {
-      answer = fnum + snum;
-      System.out.println(fnum +" "+sign +" "+snum +"= "+" "+ answer );
-
-    }
-
-    else if(sign == '-') {
-        answer = fnum - snum;
-      System.out.println(fnum +" "+sign +" "+snum +"= "+" "+ answer );
-        
-    }
-
-    else if(sign == '/') {
-        answer = fnum / snum;
-      System.out.println(fnum +" "+sign +" "+snum +"= "+" "+ answer );
-        
-    }
-
-    else if(sign == '*' ) {
-        answer = fnum * snum;
-        System.out.println(fnum +" "+sign +" "+snum +"= "+" "+ answer );
-
-        if (sign == '=') {
-          break ;
-        }
-    }
-    
-    else
-			
-        System.out.println("Your Input is not correct,please check it for any error(s).");
+                
+                }
+            } 
         
     
 
-
-
-
-    
-
-
-
-
-
-
-
-
+    public static void printResult(double fnum, char sign, double snum, double answer) {
+       // move 'System.out.println' here .
+    }
 }
-    
 
 
-
-
-
-
-
-
-
-}
-}
